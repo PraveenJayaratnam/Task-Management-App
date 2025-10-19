87330613b1e3a3c7edb839e47ab992b737ece845
Praveen Jayaratnam <praveen18.warner@gmail.com>
2025-10-19 22:51:43 +0530

    fix

--- src/app/features/task-management/components/task-management/task-management.component.ts
+++ src/app/features/task-management/components/task-management/task-management.component.ts
@@ -27,7 +27,6 @@ export class TaskManagementComponent implements OnInit, OnDestroy {
   messageType = signal<MessageType>(MessageType.Info);
 
   #taskService = inject(TaskService);
-  #dialog = inject(MatDialog);
   #subscriptions = new Set<Subscription>();
 
   #appliedFilter: TaskFilter = {
